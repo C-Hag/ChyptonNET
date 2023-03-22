4dbec82a3a4d49a084d395ee3e3c807135030ef6
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-03-22 21:37:27 +0100

    Create ifIwouldteachpythonbasics.py
    Doing some python basics in order to learn som ethical hacking stuff.

---
+++ ifIwouldteachpythonbasics.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+#Print string
+print("Hello, world!")
+print('Hello, world!')
+print("""This string runs
+multiple lines!""") #Triple quote for multi-line
+print("This string is "+"awesome!") #We can also concatenate
+print('\n') #new line
+print('Test that new line out.')
