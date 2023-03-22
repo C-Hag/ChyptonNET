e614e3cb97f83c76617825b6fdf45bd7279cac69
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-03-22 21:58:52 +0100

    Continue

--- ifIwouldteachpythonbasics.py
+++ ifIwouldteachpythonbasics.py
@@ -8,3 +8,29 @@ multiple lines!""") #Triple quote for multi-line
 print("This string is "+"awesome!") #We can also concatenate
 print('\n') #new line
 print('Test that new line out.')
+
+
+
+#MATH BASICS
+print(50 + 50) #add
+print(50 - 50) #subtract
+print(50 * 50) #multiply
+print(50 / 50) #divide
+print(50 + 50 - 50 * 50 / 50) #PEMDAS
+print(50 ** 2) #exponents 
+print(50 % 6) #modulo takes what is left over
+print(50 / 6) #division with remainder (or a float).
+print(50 // 6) #No remainder
+
+print('\n') 
+#VARIABLES AND METHODS
+quote = "The way to get started is to quit talking and begin doing."
+print(quote)
+
+print(quote.upper()) #uppercase
+print(quote.lower()) #lowercase
+print(quote.title()) #title case
+print(len(quote)) #counts characters
+
+
+
