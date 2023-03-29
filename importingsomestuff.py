96e8de1cd78334b6acbcd458ae3f5120ee833b61
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-03-29 21:13:58 +0200

    update

---
+++ importingsomestuff.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+#IMPORTING - Importing is important
+import sys
+print(sys.version)
+
+#argv python3 portscanner.py <ip>  
+#os
+#sys.exit
+#The above will be important further on. 
