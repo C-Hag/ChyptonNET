adbfa382c9f28f4e064c3ac5f7431bdccd01ff4e
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-03-29 22:01:32 +0200

    lolupdate

--- scannerlol.py
+++ scannerlol.py
@@ -12,3 +12,9 @@ if len(sys.argv) == 2:
 else: 
         print("Not enough arguments. Invalid.")
         print("Syntax: python3 scannerlol.py <ip>")
+
+#Add a banner
+print("_" * 50)
+print("Scanning target: "+target)
+print("Time started: "+str(datetime.now()))
+print("_" * 50)
