4aa90a663cb72f89fe2195a778d01ef66d0e090a
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-03-29 22:11:52 +0200

    Lolagain

--- scannerlol.py
+++ scannerlol.py
@@ -18,3 +18,25 @@ print("_" * 50)
 print("Scanning target: "+target)
 print("Time started: "+str(datetime.now()))
 print("_" * 50)
+
+try: 
+        
+        for port in range(50,85) #common ports in home routers with open DNS.
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                socket.setdefaulttimeout(1)
+                result = s.connect_ex((target.port))
+                if result == 0:
+                        print(f"Port {port} is open")
+                s.close()
+
+except KeyboardInterrupt:
+        print("\nExiting program.")
+        sys.exit()
+
+except socket.gaierror:
+        print("Hostname could no be resolved.")
+        sys.exit()
+        
+except socket.error:
+        print("Could not connect to server.")
+        sys.exit()
