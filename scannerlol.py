ed3970682289d0d6ccc97370aa6883e95d454dd0
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-03-29 21:55:30 +0200

    lol2

--- scannerlol.py
+++ scannerlol.py
@@ -6,4 +6,9 @@ import socket
 from datetime import datetime
 
 #explain/define the target. 
-if len(sys.argv) == 2:  
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
+
+else: 
+        print("Not enough arguments. Invalid.")
+        print("Syntax: python3 scannerlol.py <ip>")
