5569a6845cb1c1f9b95f88a5797ea4515d369063
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-03-29 21:19:20 +0200

    update

--- importingsomestuff.py
+++ importingsomestuff.py
@@ -2,12 +2,15 @@
 
 #IMPORTING - Importing is important
 import sys #System, functions and parameters. 
+from datetime import datetime as dt
 
 print(sys.version)
+print(datetime.now())
 
 #argv python3 portscanner.py <ip>  
 #os
 #sys.exit
 #The above will be important further on. 
+
 
 
