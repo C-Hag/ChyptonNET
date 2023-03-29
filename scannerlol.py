d8064c080133957971e31ea309532eeeb4fc615e
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-03-29 21:46:33 +0200

    LOL

---
+++ scannerlol.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+#This will probably be so bad but let's try it out. 
+
+import sys 
+import socket
+from datetime import datetime
+
+#explain/define the target. 
+if len(sys.argv) == 2:  
