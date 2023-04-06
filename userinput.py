3d933f79ed6e3fb3527d7103e6cb1e9a9b8b8ad8
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-04-06 23:15:01 +0200

    Updates!

---
+++ userinput.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+# name=input("Enter your name: ")
+# car = input("What is your favourite car? ")
+# print("Hello "+name+"! Let's go for a drive with the "+car+". ")
+
+# Let's try a simple calculator. 
+x = input("Give me a number: ")
+y = input("Give me yet another number: ")
+
+print(x + y)
+
