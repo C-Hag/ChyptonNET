2f5df3d8c77e2f250b3b3c7649e2279d7fc5ae9c
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-05-04 21:46:06 +0200

    Updated

---
+++ Employeeclassimportfile.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+from Employees import Employees
+
+e1 = Employees("Bart", "Sales", "Director of Sales", 100000, 20)
+e2 = Employees("Marge", "Executive", "CIO", 150000, 10)
+
+print(e1.name) 
+print(e2.role)
